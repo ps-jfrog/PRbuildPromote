498b9e43336d5b298a77335fece58ff88b2fd591
Krishna Manchikalapudi <krishnam@jfrog.com>
2026-04-10 20:50:17 -0700

    merge scripts fix

--- scripts/pr-dev2test.sh
+++ scripts/pr-dev2test.sh
@@ -3,6 +3,7 @@ git pull origin dev
 git checkout test
 
 git pull origin test
+echo "TEST branch short sha: $(git rev-parse --short origin/test)"
 echo "Create PR from dev to test"
 # Create Promotion Branch
 git checkout -b promote/dev-to-test
@@ -12,8 +13,13 @@ git push origin promote/dev-to-test
 
 # Merge into test
 git checkout test
+echo "TEST branch short sha: $(git rev-parse --short origin/test)"
+
 git merge --no-ff promote/dev-to-test -m "Promote dev to test"
 git push origin test
+echo "MAIN branch short sha: $(git rev-parse --short origin/main)"
+echo "TEST branch short sha: $(git rev-parse --short origin/test)"
+echo "DEV branch short sha: $(git rev-parse --short origin/dev)"
 
 # Cleanup 
 git branch -d promote/dev-to-test
